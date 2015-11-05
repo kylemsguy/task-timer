32aa042814e57dca81c8116a6beea372ccc8caf8
Kyle Zhou <kylezhou2002@hotmail.com>
2015-11-05 02:10:40 -0500

    fixed errors for python3 compatibility

--- marking-timer.py
+++ marking-timer.py
@@ -9,38 +9,39 @@ date_format = "%a %b %d %H:%M:%S %Z %Y"
 start_time = 0
 
 def sighup_handler(signum, frame):
-	write_end_time()
-	quit()
+    write_end_time()
+    quit()
 
 def write_start_time():
-	global start_time
-	print("Writing start time to {0}...").format(filename)
-	start_time = datetime.now()
-	with open(filename, "a") as hoursfile:
-		hoursfile.write("Start time: ")
-		hoursfile.write(time.strftime(date_format))
-		hoursfile.write('\n')
+    global start_time
+    print("Writing start time to {0}...".format(filename))
+    start_time = datetime.now()
+    with open(filename, "a") as hoursfile:
+        hoursfile.write("Start time: ")
+        hoursfile.write(time.strftime(date_format))
+        hoursfile.write('\n')
 
 def write_end_time():
-	print("Writing end time to {0}...").format(filename)
-	end_time = datetime.now()
-	time_marked = end_time - start_time
-	with open(filename, "a") as hoursfile:
-		hoursfile.write("End time: ")
-		hoursfile.write(time.strftime(date_format))
-		hoursfile.write('\n')
-		hoursfile.write("Total time marked: ")
-		hoursfile.write(str(time_marked).split('.')[0])
-		hoursfile.write("\n\n")
+    print("Writing end time to {0}...".format(filename))
+    end_time = datetime.now()
+    time_marked = end_time - start_time
+    with open(filename, "a") as hoursfile:
+        hoursfile.write("End time: ")
+        hoursfile.write(time.strftime(date_format))
+        hoursfile.write('\n')
+        hoursfile.write("Total time marked: ")
+        hoursfile.write(str(time_marked).split('.')[0])
+        hoursfile.write("\n\n")
 
 if __name__ == "__main__":
-	write_start_time()
-	# Set up SIGHUP handler
-	signal.signal(signal.SIGHUP, sighup_handler)
-	try:
-		print("Waiting until you finish your marking session.")
-		print("When you are done, either press Ctrl-C or simply log off of the SSH connection.")
-		while(True): pass # hang until user quits
-	except KeyboardInterrupt:
-		print("Writing end time...")
-		write_end_time()
+    write_start_time()
+    # Set up SIGHUP handler
+    signal.signal(signal.SIGHUP, sighup_handler)
+    try:
+        print("Waiting until you finish your marking session.")
+        print("When you are done, either press Ctrl-C, enter, or simply end the SSH connection.")
+        input() # hang until user quits
+    except KeyboardInterrupt:
+        pass
+    finally:
+        write_end_time()
