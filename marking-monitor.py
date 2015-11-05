10ff9a6f6ce0f12e7e87eae1bc1981a25b3179bc
Kyle Zhou <kylezhou2002@hotmail.com>
2015-11-05 02:00:50 -0500

    added signal handler for SIGHUP

--- marking-monitor.py
+++ marking-monitor.py
@@ -1,11 +1,24 @@
+#!/usr/bin/env python3
 import time
+import signal
 from datetime import datetime
 
 filename = "hours-marking.txt"
 date_format = "%a %b %d %H:%M:%S %Z %Y"
 
 start_time = 0
-end_time = 0
+
+def sighup_handler(signum, frame):
+	write_end_time()
+
+def write_start_time():
+	global start_time
+	print("Writing start time to {0}...").format(filename)
+	start_time = datetime.now()
+	with open(filename, "a") as hoursfile:
+		hoursfile.write("Start time: ")
+		hoursfile.write(time.strftime(date_format))
+		hoursfile.write('\n')
 
 def write_end_time():
 	print("Writing end time to {0}...").format(filename)
@@ -19,24 +32,14 @@ def write_end_time():
 		hoursfile.write(str(time_marked).split('.')[0])
 		hoursfile.write("\n\n")
 
-
-def main():
-	print("Writing start time to {0}...").format(filename)
-	start_time = datetime.now()
-	with open(filename, "a") as hoursfile:
-		hoursfile.write("Start time: ")
-		hoursfile.write(time.strftime(date_format))
-		hoursfile.write('\n')
-
-	print("Waiting until you finish your marking session.")
-	print("When you are done, either press Ctrl-C or simply log off of the SSH connection.")
-
-	while(True): pass # hang until user quits
-
-
 if __name__ == "__main__":
+	write_start_time()
+	# Set up SIGHUP handler
+	signal.signal(signal.SIGHUP, sighup_handler)
 	try:
-		main()
+		print("Waiting until you finish your marking session.")
+		print("When you are done, either press Ctrl-C or simply log off of the SSH connection.")
+		while(True): pass # hang until user quits
 	except KeyboardInterrupt:
 		print("Writing end time...")
 		write_end_time()
