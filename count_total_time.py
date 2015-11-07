f987354cda132d399f8500b20fa3bb71eabcd248
Kyle Zhou <kylezhou2002@hotmail.com>
2015-11-07 00:47:28 -0500

    added time counter as well

---
+++ count_total_time.py
@@ -0,0 +1,20 @@
+import datetime
+
+def parse_time(timestr):
+    time_split = timestr.split(":")
+    timedelta = datetime.timedelta(hours=int(time_split[0]), minutes=int(time_split[1]), seconds=int(time_split[2]))
+    return timedelta
+
+
+def main():
+    totalcount = datetime.timedelta()
+    with open("hours-marking.txt", 'r') as infile:
+        lines = infile.readlines()
+        for line in lines:
+            if "Total time marked:" in line:
+                time_raw = line.split(" ")[3]
+                timedelta = parse_time(time_raw)
+                totalcount += timedelta
+    print(str(totalcount).split('.')[0])
+if __name__ == "__main__":
+    main()
